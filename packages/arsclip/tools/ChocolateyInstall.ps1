64b3613be4ef68204d4f65b599ba9bb4ef8daa86
Tobias Wallura <tobias@wallura.org>
2016-03-07 23:58:23 +0100

    add ArsClip package

---
+++ packages/arsclip/tools/ChocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName  = 'arsclip'
+$tempDir      = Join-Path $env:TEMP $packageName
+$url          = 'http://www.joejoesoft.com/cms/file.php?f=userupload%2f8%2ffiles%2facv507.zip'
+$fileLocation = Join-Path $tempDir 'setup.exe'
+$silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$options = @{
+  Headers = @{
+    Referer = 'http://www.joejoesoft.com/vcms/97/';
+  }
+}
+
+Install-ChocolateyZipPackage $packageName $url $tempDir -options $options
+Install-ChocolateyInstallPackage $packageName 'exe' $silentArgs $fileLocation
