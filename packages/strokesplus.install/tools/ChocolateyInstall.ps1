f3ae5aa7127ed2ffcf1808ab3df41b0488caa8d2
Tobias Wallura <tobias@wallura.org>
2016-03-21 21:18:42 +0100

    add strokesplus.install package

---
+++ packages/strokesplus.install/tools/ChocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'strokesplus.install'
+$url         = 'http://www.strokesplus.com/files/StrokesPlusSetup_2.8.6.4_x86_Signed.exe'
+$url64       = 'http://www.strokesplus.com/files/StrokesPlusSetup_2.8.6.4_x64_Signed.exe'
+$silentArgs  = "/SP- /VERYSILENT /SUPPRESSMSGBOXES"
+
+Install-ChocolateyPackage $packageName 'EXE' $silentArgs $url $url64
