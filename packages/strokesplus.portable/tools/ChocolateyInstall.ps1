a758bf41aa8a8f7ea58dc5903af4cab0c8317468
Tobias Wallura <tobias@wallura.org>
2016-03-21 21:19:06 +0100

    update strokesplus.portable to 2.8.6.4

--- packages/strokesplus.portable/tools/ChocolateyInstall.ps1
+++ packages/strokesplus.portable/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'strokesplus.portable'
 $toolsDir    = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$url         = 'http://www.strokesplus.com/files/StrokesPlus_2.8.6.3_x86.zip'
-$url64       = 'http://www.strokesplus.com/files/StrokesPlus_2.8.6.3_x64.zip'
+$url         = 'http://www.strokesplus.com/files/StrokesPlus_2.8.6.4_x86.zip'
+$url64       = 'http://www.strokesplus.com/files/StrokesPlus_2.8.6.4_x64.zip'
 $zipFolder   = if (Get-ProcessorBits 32) { 'StrokesPlus_x86' } else { 'StrokesPlus_x64' }
 $rmZipFolder = "${zipFolder}_Signed"
 
