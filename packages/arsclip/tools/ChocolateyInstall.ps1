a9d0f12c5c49988615baf208e2a1ec7802cc7119
Tobias Wallura <tobias@wallura.org>
2016-05-08 22:40:28 +0200

    update Arsclip to 5.08

--- packages/arsclip/tools/ChocolateyInstall.ps1
+++ packages/arsclip/tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName  = 'arsclip'
 $tempDir      = Join-Path $env:TEMP $packageName
-$url          = 'http://www.joejoesoft.com/cms/file.php?f=userupload%2f8%2ffiles%2facv507.zip'
+$url          = 'http://www.joejoesoft.com/cms/file.php?f=userupload%2f8%2ffiles%2facv508.zip'
 $fileLocation = Join-Path $tempDir 'setup.exe'
 $silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $options = @{
