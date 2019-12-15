28e265b4121b5fe4646105a25f149a4142c0b1c6
Tobias Wallura <tobias@wallura.org>
2019-12-15 18:22:47 +0100

    fix resolving download URL for arsclip

--- packages/arsclip/tools/ChocolateyInstall.ps1
+++ packages/arsclip/tools/ChocolateyInstall.ps1
@@ -1,12 +1,13 @@
 ﻿$packageName  = 'arsclip'
 $tempDir      = Join-Path $env:TEMP $packageName
-$url          = 'http://www.joejoesoft.com/cms/file.php?f=userupload/8/files/acv528.zip'
+$url          = 'https://www.joejoesoft.com/cms/file.php?f=userupload/8/files/acv528.zip'
 $checksum     = '563df68019ed8dd25b25ed93148bfe0ea0f9427d242be5e736c49f954a380d4f'
 $fileLocation = Join-Path $tempDir 'setup.exe'
 $silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
 # Resolve download link
-$url = (Invoke-WebRequest -Uri $url).ParsedHtml.querySelector(".content a").href
+$urlWithToken = ((Invoke-WebRequest -Uri $url -UseBasicParsing).Links | Where-Object { $_.href -Like "*.zip*" }).href.Replace("&amp;", "&")
+$url = (New-Object System.Uri -ArgumentList @([System.Uri]$url, $urlWithToken)).AbsoluteUri
 Write-Verbose "Resolved download URL to $url"
 
 Install-ChocolateyZipPackage -PackageName $packageName -Url $url -UnzipLocation $tempDir -Checksum $checksum -ChecksumType 'sha256'
