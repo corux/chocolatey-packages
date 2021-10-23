3d3164e1edc46154a175fdec8663b3965748f7da
Joel Bennett <Jaykul@HuddledMasses.org>
2021-10-23 17:43:01 -0400

    Fix Resolving Download URL
    Don't loose the anti-bot token ;-)

--- packages/arsclip/tools/ChocolateyInstall.ps1
+++ packages/arsclip/tools/ChocolateyInstall.ps1
@@ -1,13 +1,13 @@
-﻿$packageName  = 'arsclip'
+$packageName  = 'arsclip'
 $tempDir      = Join-Path $env:TEMP $packageName
-$url          = 'https://www.joejoesoft.com/cms/file.php?f=userupload/8/files/acv531.zip'
-$checksum     = '34bdce7f040c3eabe783727223d62f0282906fd16996cacaac035de374a32719'
+$url          = 'http://www.joejoesoft.com/cms/file.php?f=userupload/8/files/acv533.zip'
+$checksum     = 'edbded2c0dae0896c772b00af466318cbfc897eb0eeaf7987c5c6470c1a1fd0a'
 $fileLocation = Join-Path $tempDir 'setup.exe'
 $silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
 # Resolve download link
 $urlWithToken = ((Invoke-WebRequest -Uri $url -UseBasicParsing).Links | Where-Object { $_.href -Like "*.zip*" }).href.Replace("&amp;", "&")
-$url = (New-Object System.Uri -ArgumentList @([System.Uri]$url, $urlWithToken)).AbsoluteUri
+$url = (New-Object System.Uri -ArgumentList @($url, $urlWithToken, $true)).AbsoluteUri
 Write-Verbose "Resolved download URL to $url"
 
 Install-ChocolateyZipPackage -PackageName $packageName -Url $url -UnzipLocation $tempDir -Checksum $checksum -ChecksumType 'sha256'
